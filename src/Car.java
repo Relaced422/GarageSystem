56c8c0dca180d35887f2eb69dde692863527e898
Relaced422 <Optquenum@gmail.com>
2026-05-28 11:03:13 +0200

    Added files and basic car class

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    private String licensePlate;
+    private String brand;
+    private String model;
+    private String colour;
+
+    public Car(String licensePlate, String brand, String model, String colour) {
+        this.licensePlate = licensePlate;
+        this.brand = brand;
+        this.model = model;
+        this.colour = colour;
+    }
+
+    public String getLicensePlate() { return licensePlate; }
+    public String getBrand()        { return brand; }
+    public String getModel()        { return model; }
+    public String getColour()       { return colour; }
+}
